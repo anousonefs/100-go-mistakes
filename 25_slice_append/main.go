018514b01a9f303363545f171a961fb5e4d980ad
anousone <anousone.litd@gmail.com>
2024-08-13 14:46:51 +0700

    .

--- 25_slice_append/main.go
+++ 25_slice_append/main.go
@@ -9,24 +9,25 @@ func main() {
 	// s1 = 1,2,10, s2 = 2, s3 = 2, 10
 	fmt.Printf("s1: %v, s2: %v, s3: %v\n\n", s1, s2, s3)
 
-	updateSlice()
-	updateSlice2()
+	updateSlice()  // wrong
+	updateSlice2() // correct
 	updateSlice3()
 }
 
 func updateSlice() {
 	s := []int{1, 2, 3}
-	f(s[0:2])
+	f(s[0:2])                        // len: 2, cap: 3
 	fmt.Printf("update v1: %v\n", s) // [9 2 10]
 }
 
+// update just first 2 elements
 func updateSlice2() {
-	s := []int{1, 2, 3}
+	s := []int{1, 2, 3, 4, 5}
 	sCopy := make([]int, 2)
-	copy(sCopy, s)
+	copy(sCopy, s) // len: 2, cap: 2
 	f(sCopy)
-	result := append(sCopy, s[2])
-	fmt.Printf("update v2: %v\n", result) // [9 2 3]
+	result := append(sCopy, s[2:]...)
+	fmt.Printf("update v2: %v\n", result) // [9 2 3 4 5]
 }
 
 func f(s []int) {
@@ -39,7 +40,7 @@ func updateSlice3() {
 	s := []int{1, 2, 3}
 	sCopy := make([]int, 2)
 	copy(sCopy, s)
-	sCopy = f3(sCopy)
+	sCopy = f3(sCopy) // whan f3() append effect
 	result := append(sCopy, s[2])
 	fmt.Printf("update v3: %v\n", result) // [9, 2, 10, 3]
 }
