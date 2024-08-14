352f42583a59eb0c552979c49904d859ee8abb2d
anousone <anousone.litd@gmail.com>
2024-08-14 14:34:38 +0700

    26 slices and memory leaks

--- 26_slices_and_memory_leaks/main.go
+++ 26_slices_and_memory_leaks/main.go
@@ -1,7 +1,13 @@
 package main
 
+import (
+	"fmt"
+	"runtime"
+)
+
 func main() {
-	consumeMessages()
+	/* consumeMessages() */
+	sliceAndPointer()
 }
 
 func consumeMessages() {
@@ -27,4 +33,50 @@ func receiveMessage() []byte {
 }
 
 func storeMessageType(_ []byte) {
+}
+
+// slice and pointers
+
+func printAlloc() {
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+	fmt.Printf("%d KB\n", m.Alloc/1024)
+}
+
+type Foo struct {
+	v []byte
+}
+
+func sliceAndPointer() {
+	foos := make([]Foo, 1_000)
+	printAlloc()
+	for i := 0; i < len(foos); i++ {
+		foos[i] = Foo{
+			v: make([]byte, 1024*1024),
+		}
+	}
+	printAlloc()
+	two := keepFirstTwoElementsOnly2(foos)
+	runtime.GC()
+	printAlloc()
+	runtime.KeepAlive(two)
+}
+
+func keepFirstTwoElementsOnly(foos []Foo) []Foo {
+	return foos[:2]
+}
+
+// use slice copy
+func keepFirstTwoElementsOnly2(foos []Foo) []Foo {
+	res := make([]Foo, 2)
+	copy(res, foos)
+	return res
+}
+
+// make pointer field to nil
+func keepFirstTwoElementsOnly3(foos []Foo) []Foo {
+	for i := 2; i < len(foos); i++ {
+		foos[i].v = nil
+	}
+	return foos[:2]
 }
