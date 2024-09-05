d47d257c0c2ae513ce8bb8038425682f3431cafd
anousone <anousone.litd@gmail.com>
2024-09-05 16:28:14 +0700

    82 not categorizing tests

---
+++ 82_not_categorizing_tests/main.go
@@ -0,0 +1,9 @@
+package main
+
+func main() {
+	/*
+	   1. using build tags at the test file level
+	   2. Using environment variables to mark a specific test
+	   3. Based on the test pace using short mode
+	*/
+}
